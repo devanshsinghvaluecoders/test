4136722914e0f958265901c7ad0cd2d4c6d75549
devansh <devanshsingh5656>
2021-06-04 19:34:18 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  // const [val, setval] = useState([]);
+  // const [pages, setpages] = useState(1);
+  // const infiniteScroll = () => {
+  //   if (
+  //     window.innerHeight + document.documentElement.scrollTop >=
+  //     document.documentElement.offsetHeight
+  //   ) {
+  //     setpages(pages + 1);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   window.addEventListener("scroll", infiniteScroll);
+  // }, [document.documentElement.scrollTop]);
+
+  // useEffect(() => {
+  //   fetch(`https://jsonplaceholder.typicode.com/posts`)
+  //     .then((res) => res.json())
+  //     .then((data) => setval([...val, ...data]));
+  // }, [pages]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {val?.map((res) => (
+        <div>{res.title}</div>
+      ))} */}
     </div>
   );
 }
